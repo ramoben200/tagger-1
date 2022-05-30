08fcb09e027d028ec47e799aa0c44f019d568fb4
SakirBey1 <sakirhack81@gmail.com>
2022-05-30 17:28:25 +0300

    Update mentionall.py

--- mentionall.py
+++ mentionall.py
@@ -3,6 +3,7 @@ from telethon import Button
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 from telethon.tl.types import ChannelParticipantsAdmins
+from 
 
 logging.basicConfig(
     level=logging.INFO,
