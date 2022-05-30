118ce3f8a3266fa6d15dd3b91ec9ed17e9381a49
alihqn11 <106037814+alihqn11@users.noreply.github.com>
2022-05-30 22:16:53 +0300

    Update mentionall.py

--- mentionall.py
+++ mentionall.py
@@ -3,7 +3,7 @@ from telethon import Button
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 from telethon.tl.types import ChannelParticipantsAdmins
-import bot_username, support, owner
+from config import bot_username, support, owner
 
 logging.basicConfig(
     level=logging.INFO,
