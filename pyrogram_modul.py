313c4bfb1b74abb962246e483d30d95a3c8151f2
alihqn11 <106037814+alihqn11@users.noreply.github.com>
2022-06-12 03:03:39 +0300

    Update pyrogram_modul.py

--- pyrogram_modul.py
+++ pyrogram_modul.py
@@ -30,10 +30,11 @@ async def hg(bot: Client, msg: Message):
 @app.on_message(filters.command("id"))
 async def _id(_, message: Message):
     msg = message.reply_to_message or message
-    out_str = f" Grup ID : {(msg.forward_from_chat or msg.chat).id}\n"
-    out_str += f" Mesaj ID : {msg.forward_from_message_id or msg.message_id}\n"
+    out_str = "**User İnfo:**"
+    out_str += f" ⚡️Grup ID : {(msg.forward_from_chat or msg.chat).id}\n"
+    out_str += f" 💬Mesaj ID : {msg.forward_from_message_id or msg.message_id}\n"
     if msg.from_user:
-        out_str += f" Yanıtlanan Kullanıcı : {msg.from_user.id}\n"
+        out_str += f" 🙋🏻‍♂️Yanıtlanan Kullanıcı : {msg.from_user.id}\n"
  
     await message.reply(out_str)
 
